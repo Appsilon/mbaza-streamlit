6bef4973ee32a63f36d407553c8f96f23111de28
Piotr Pasza Storozenko <piotr@appsilon.com>
2023-10-23 16:22:26 +0200

    Git init

---
+++ app.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import streamlit as st
+from PIL import Image
+
+from model import Model
+
+st.set_page_config(page_title="Mbaza Streamlit Interface")
+
+
+@st.cache_resource
+def get_model():
+    return Model("resnet50_larger.onnx")
+
+
+model = get_model()
+
+"# Mbaza Streamlit Interface"
+"By [Appsilon](https://appsilon.com/) with ❤️"
+"---"
+"This app is a simple showcase of how to run [Mbaza](https://appsilon.com/data-for-good/mbaza-ai/) ONNX model in `streamlit`."
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+options = ["Upload your own"] + sorted(Path("images").glob("*.jpg"))
+selected_predefined_image = st.selectbox("Or select a predefined image", options)
+
+img = None
+if uploaded_file and selected_predefined_image == "Upload your own":
+    img = Image.open(uploaded_file)
+    st.image(img, caption="Uploaded Image", use_column_width=True)
+if selected_predefined_image is not None and selected_predefined_image != "Upload your own":
+    img = Image.open(selected_predefined_image)
+    st.image(img, caption=f"Predefined {selected_predefined_image}", use_column_width=True)
+
+if img:
+    label, confidence = model.predict(img)
+    f"Classification: {label}"
+    f"Confidence: {confidence:.2f}"
---
+++ model.py
@@ -0,0 +1,53 @@
+import onnxruntime as ort
+import numpy as np
+from PIL import Image
+
+
+class Model:
+    def __init__(self, path) -> None:
+        self.session = ort.InferenceSession(path)
+        self.img_size = self.session.get_inputs()[0].shape[2:][::-1]
+        self.classes = [
+            "Bird",
+            "Blank",
+            "Buffalo_African",
+            "Cat_Golden",
+            "Chevrotain_Water",
+            "Chimpanzee",
+            "Civet_African_Palm",
+            "Duiker_Blue",
+            "Duiker_Red",
+            "Duiker_Yellow_Backed",
+            "Elephant_African",
+            "Genet",
+            "Gorilla",
+            "Guineafowl_Black",
+            "Guineafowl_Crested",
+            "Hog_Red_River",
+            "Human",
+            "Leopard_African",
+            "Mandrillus",
+            "Mongoose",
+            "Mongoose_Black_Footed",
+            "Monkey",
+            "Pangolin",
+            "Porcupine_Brush_Tailed",
+            "Rail_Nkulengu",
+            "Rat_Giant",
+            "Rodent",
+            "Squirrel",
+        ]
+
+    def _preprocess_image(self, img: Image.Image):
+        img = img.resize(self.img_size).convert("RGB")
+        x = np.array(img).astype(np.float32) / 255
+        x = np.expand_dims(x, 0)
+        x = np.moveaxis(x, -1, 1)
+        return x
+
+    def predict(self, img: Image.Image):
+        A = self._preprocess_image(img)
+        y_pred = self.session.run(None, {"input": A})[0][0]
+        label = self.classes[np.argmax(y_pred)]
+        confidence = y_pred.max()
+        return label, confidence
